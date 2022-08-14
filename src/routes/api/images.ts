1a5874d725df684f803229fd519637680ec7e013
Hussein Samy <hussein.samy02@gmail.com>
2022-08-14 23:49:31 +0200

    Refactor the /images endpoint

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,21 +1,33 @@
 import express from 'express'
 import {promises as fs} from 'fs'
+import sharp, { ResizeOptions } from 'sharp'
 
 const images = express.Router();
 
-// const createThumb = (image) => {
+const createThumb = (image: Buffer, width: number, height: number, filename: string, other: ResizeOptions) => {
+    sharp(image).resize(width,height,other).toBuffer((err, buffer) => {
+        try{
 
-// }
+            fs.writeFile(`C:/Users/Weshkl/Desktop/Image processing API/assets/thumbs/${filename}.jpg`, buffer);
+        }
+        catch (err){
+            console.log(err)
+        }
 
-images.get('/', (req, res)=>{
-    try
-    {
-        res.sendFile(`C:/Users/Weshkl/Desktop/Image processing API/assets/full/${req.query.filename}.jpg`);
+    })
+}
+ 
+images.get('/', async(req, res)=>{
+    const {filename, width, height, ...other} = req.query; 
+    const imagePath = `C:/Users/Weshkl/Desktop/Image processing API/assets/full/${filename}.jpg`
+    const resizedImagePath = `C:/Users/Weshkl/Desktop/Image processing API/assets/thumbs/${filename}.jpg`
+    const image: Buffer = await fs.readFile(imagePath);
+    if(await fs.readFile(resizedImagePath))  
+        res.sendFile(resizedImagePath);
+    else{
+        createThumb(image, +width!, +height!, filename! as string, other as ResizeOptions);
+        res.sendFile(resizedImagePath);
     }
-    catch (err) {
-        res.send(err)
-    }
-
 });
 
 export default images;
